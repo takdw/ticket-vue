11989b5ba4884d7512c089db5f3c18ab06cec824
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2020-11-20 16:00:29 +0300

    fetch tickets from api for the homepage

---
+++ src/components/TicketCard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <router-link class="block h-full" :to="`/ticket/${ticket.id}`">
+      <div
+        class="relative h-full bg-white rounded-lg shadow hover:shadow-xl overflow-hidden transition ease-in-out duration-300 cursor-pointer"
+      >
+        <div class="absolute top-0 left-0">
+          <div class="ml-4 mt-4 bg-white bg-opacity-25 px-2 py-1 rounded-lg">
+            <p class="font-semibold">
+              <span class="uppercase text-sm text-gray-200">etb</span>
+              <span class="ml-1 text-xl text-white">{{ price }}</span>
+            </p>
+          </div>
+        </div>
+        <div class="h-56 overflow-hidden">
+          <img
+            class="w-full object-fit"
+            src="https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+            :alt="ticket.title"
+          />
+        </div>
+        <div class="flex p-6 space-x-6">
+          <div class="">
+            <p class="text-center leading-tight">
+              <span class="text-indigo-500 font-semibold uppercase">
+                {{ month }}
+              </span>
+              <br />
+              <span class="text-4xl font-semibold">
+                {{ day }}
+              </span>
+            </p>
+          </div>
+          <div>
+            <h2 class="text-xl font-semibold">
+              {{ ticket.title }}
+            </h2>
+            <p class="mt-2 leading-tight text-gray-700">
+              {{ ticket.subtitle }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { DateTime } from "luxon";
+
+export default {
+  props: ["ticket"],
+  data: () => ({
+    date: {},
+  }),
+  created() {
+    this.date = DateTime.fromISO(this.ticket.date);
+  },
+  computed: {
+    month() {
+      return this.date.toFormat("LLL");
+    },
+    day() {
+      return this.date.toFormat("d");
+    },
+    price() {
+      return (this.ticket.price / 100).toFixed(2);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,60 +21,31 @@
     <div class="container mx-auto">
       <h2 class="text-2xl font-bold text-gray-700">Upcoming Events</h2>
       <div class="grid grid-cols-3 gap-12 py-6">
-        <router-link to="/ticket/1" v-for="i in 9" :key="`event-grid-${i}`">
-          <div
-            class="relative bg-white rounded-lg shadow hover:shadow-xl overflow-hidden transition ease-in-out duration-300 cursor-pointer"
-          >
-            <div class="absolute top-0 left-0">
-              <div
-                class="ml-4 mt-4 bg-white bg-opacity-25 px-2 py-1 rounded-lg"
-              >
-                <p class="font-semibold">
-                  <span class="uppercase text-sm text-gray-200">etb</span>
-                  <span class="ml-1 text-xl text-white">200</span>
-                </p>
-              </div>
-            </div>
-            <div class="h-56 overflow-hidden">
-              <img
-                class="w-full object-fit"
-                src="https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-                alt="Event Poster"
-              />
-            </div>
-            <div class="flex p-6 space-x-6">
-              <div class="">
-                <p class="text-center leading-tight">
-                  <span class="text-indigo-500 font-semibold uppercase">
-                    Sep
-                  </span>
-                  <br />
-                  <span class="text-4xl font-semibold">16</span>
-                </p>
-              </div>
-              <div>
-                <h2 class="text-xl font-semibold">
-                  Lorem ipsum dolor sit amet
-                </h2>
-                <p class="mt-2 leading-tight text-gray-700">
-                  Soluta quo necessitatibus recusandae sapiente consequuntur.
-                  Laboriosam at officia, explicabo tempore.
-                </p>
-              </div>
-            </div>
-          </div>
-        </router-link>
+        <TicketCard
+          v-for="(ticket, index) in tickets"
+          :key="`event-grid-${index}`"
+          :ticket="ticket"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import TicketCard from "@/components/TicketCard";
+
 export default {
-  computed: {
-    year() {
-      return new Date().getFullYear();
-    },
+  components: {
+    TicketCard,
+  },
+  data: () => ({
+    tickets: [],
+  }),
+  created() {
+    this.$http
+      .get("tickets")
+      .then(response => (this.tickets = response.data))
+      .catch(err => console.log(err));
   },
 };
 </script>
