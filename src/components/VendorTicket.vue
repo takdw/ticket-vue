3116270e45e75a7920d6de918a9bdb9d80c420bf
Tamrat Assefa Kessito <mamushassefa@gmail.com>
2020-11-21 17:06:55 +0300

    turn the ticket status SVGs into dynamic items

--- src/components/VendorTicket.vue
+++ src/components/VendorTicket.vue
@@ -7,14 +7,29 @@
     </div>
     <div class="col-span-1 grid place-items-center px-6 py-4 whitespace-nowrap">
       <svg
+        v-if="published"
         class="text-green-500 w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+        :title="publishedAt"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+      <svg
+        v-else
+        class="text-gray-500 w-6 h-6"
         fill="currentColor"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
           fill-rule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
           clip-rule="evenodd"
         ></path>
       </svg>
@@ -23,7 +38,22 @@
       class="col-span-1 grid place-items-center px-6 py-4 whitespace-nowrap text-sm text-gray-500"
     >
       <svg
-        class="text-red-500 w-6 h-6"
+        v-if="approved"
+        class="text-green-500 w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+        :title="approvedAt"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+      <svg
+        v-else
+        class="text-gray-500 w-6 h-6"
         fill="currentColor"
         viewBox="0 0 20 20"
         xmlns="http://www.w3.org/2000/svg"
@@ -79,6 +109,22 @@ export default {
     },
     diff() {
       return this.dateObj.toRelative();
+    },
+    published() {
+      return this.ticket.published_at;
+    },
+    approved() {
+      return this.ticket.approved_at;
+    },
+    publishedAt() {
+      return this.published
+        ? DateTime.fromISO(this.published).toLocaleString(DateTime.DATE_FULL)
+        : "";
+    },
+    approvedAt() {
+      return this.approved
+        ? DateTime.fromISO(this.approved).toLocaleString(DateTime.DATE_FULL)
+        : "";
     },
   },
   methods: {},
